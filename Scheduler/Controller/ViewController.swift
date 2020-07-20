d1225a01f07253b046e4e7b7576397b89aca0798
Czarek <c.przygodzki@gmail.com>
2020-07-20 11:48:45 +0200

    First commit

---
+++ Scheduler/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scheduler
+//
+//  Created by Cezary Przygodzki on 19/07/2020.
+//  Copyright © 2020 PekackaPrzygodzki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
